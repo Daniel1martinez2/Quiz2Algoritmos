72586ace167a5c269486c5f57520a38756992d15
daniel m <daniel.martinezvillegas2001@gmail.com>
2020-05-08 14:03:17 -0500

    hola

---
+++ src/view/Exception1.java
@@ -0,0 +1,13 @@
+package view;
+
+public class Exception1 extends Exception{
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public Exception1(String message) {
+		super(message); 
+		
+	}
+
+}
